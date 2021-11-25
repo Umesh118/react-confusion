36a2532ab272ce1b94e7ed31a87b5d74cd64fca3
Umesh Dhangare <umesh.dhangare@nobrainsolutions.com>
2021-11-25 18:11:44 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 // import logo from "./logo.svg";
+import { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristornate Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu></Menu>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristornate Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}></Menu>
+      </div>
+    );
+  }
 }
 
 export default App;
