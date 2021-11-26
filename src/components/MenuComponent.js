61fbfccdb63a00770089413127126d44014efd72
Umesh Dhangare <umesh.dhangare@nobrainsolutions.com>
2021-11-26 16:28:21 +0530

    Assignment 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
 // import { Media } from "reactstrap";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import DishDetailComponent from "./DishDetailComponent";
 
 export class Menu extends Component {
   constructor(props) {
@@ -21,27 +15,28 @@ export class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name}></CardImg>
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText> {dish.description} </CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
+  // renderDish(dish) {
+  //   if (dish != null) {
+  //     return (
+  //       <Card>
+  //         <CardImg top src={dish.image} alt={dish.name}></CardImg>
+  //         <CardBody>
+  //           <CardTitle>{dish.name}</CardTitle>
+  //           <CardText> {dish.description} </CardText>
+  //         </CardBody>
+  //       </Card>
+
+  //     );
+  //   } else {
+  //     return <div></div>;
+  //   }
+  // }
 
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
-        <div className="col-12 col-md-5 m-1">
-          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card onClick={() => this.onDishSelect(dish)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -54,11 +49,8 @@ export class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-        </div>
+
+        <DishDetailComponent dish={this.state.selectedDish} />
       </div>
     );
   }
