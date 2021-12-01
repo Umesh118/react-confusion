5a223b8868c94ca781319b0ef8bf15a7c366f560
Umesh Dhangare <umesh.dhangare@nobrainsolutions.com>
2021-12-01 10:44:25 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,11 @@ import { Component } from "react";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import DishDetail from "./DishDetailComponent";
+import Contact from "./ContactComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { Routes, Route, Redirect } from "react-router-dom";
@@ -12,6 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
@@ -20,12 +27,28 @@ class Main extends Component {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />}></Route>
+          <Route
+            path="/home"
+            element={
+              <Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={
+                  this.state.promotions.filter(
+                    (promotion) => promotion.featured
+                  )[0]
+                }
+                leader={
+                  this.state.leaders.filter((leader) => leader.featured)[0]
+                }
+              />
+            }
+          ></Route>
           <Route
             exact
             path="/menu"
             element={<Menu dishes={this.state.dishes} />}
           ></Route>
+          <Route exact path="/contactus" element={<Contact />}></Route>
         </Routes>
 
         <Footer />
